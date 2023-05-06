6f8e2471e4458df39ef3304b1e18323f76d01e4f
luca fonzo <lucafonzo3110@gmail.com>
2023-05-05 21:27:12 -0300

    Init repo con esqueleto del tpe

---
+++ src/servicios/ServicioBFS.java
@@ -0,0 +1,21 @@
+package servicios;
+
+import grafos.Grafo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioBFS {
+
+    private Grafo<?> grafo;
+
+    public ServicioBFS(Grafo<?> grafo) {
+        this.grafo = grafo;
+    }
+
+    public List<Integer> bfsForest() {
+        // Resolver BFS
+        return new ArrayList<>();
+    }
+
+}
---
+++ src/servicios/ServicioDFS.java
@@ -0,0 +1,21 @@
+package servicios;
+
+import grafos.Grafo;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ServicioDFS {
+
+    private Grafo<?> grafo;
+
+    public ServicioDFS(Grafo<?> grafo) {
+        this.grafo = grafo;
+    }
+
+    public List<Integer> dfsForest() {
+        // Resolver DFS
+        return new ArrayList<>();
+    }
+
+}
