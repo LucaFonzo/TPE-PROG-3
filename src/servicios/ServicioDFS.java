7b6e1a80e6149abea9072e58693ead9314c487c0
Camilo Cagliolo <camilocagliolo14@gmail.com>
2023-06-28 18:08:50 -0300

    Arreglado dfs

--- src/servicios/ServicioDFS.java
+++ src/servicios/ServicioDFS.java
@@ -2,6 +2,7 @@ package servicios;
 
 import grafos.Grafo;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 
@@ -17,23 +18,25 @@ public class ServicioDFS {
     public List<Integer> dfsForest() {
         ArrayList<Integer> lista = new ArrayList<>();
         Iterator<Integer> vertices = grafo.obtenerVertices();
+        Hashtable<Integer, Integer> visitados = new Hashtable<>();
 
         while(vertices.hasNext()){ // O(V)
             Integer current = vertices.next();
-            dfsForest(current, lista);
+            dfsForest(current, lista, visitados);
         }
 
         return lista;
     }
 
-    private void dfsForest(Integer vertice, ArrayList<Integer> lista){
-        if(lista.contains(vertice)){ // O(V)
+    private void dfsForest(Integer vertice, ArrayList<Integer> lista, Hashtable<Integer, Integer> visitados){
+        if(visitados.containsKey(vertice)){ // O(V)
             return;
         }
         lista.add(vertice);
+        visitados.put(vertice, vertice);
         Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(vertice);
         while(adyacentes.hasNext()){ // O(A)
-            dfsForest(adyacentes.next(), lista); // O(V)
+            dfsForest(adyacentes.next(), lista, visitados);
         }
     }
 }
